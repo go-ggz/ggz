bd37e005c8aebbf967619fc95a019d7e0c1ba27c
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-10-02 11:31:03 +0800

    add coverage
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- helper/array_test.go
+++ helper/array_test.go
@@ -1,11 +1,37 @@
 package helper
 
-import (
-	"testing"
+import "testing"
 
-	"github.com/stretchr/testify/assert"
-)
-
-func TestDiffArray(t *testing.T) {
-
+func TestInArray(t *testing.T) {
+	type args struct {
+		needle   string
+		haystack []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"in array",
+			args{
+				needle:   "apple",
+				haystack: []string{"apple", "banana"},
+			},
+			true,
+		},
+		{
+			"not in array",
+			args{
+				needle:   "apple",
+				haystack: []string{"appleboy", "banana"},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		if got := InArray(tt.args.needle, tt.args.haystack); got != tt.want {
+			t.Errorf("%q. InArray() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
 }
