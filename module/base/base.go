5145866ccfc559632b45824a9eb733fd330d9798
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-01-06 20:40:33 +0800

    feat(model): add access token table. (#29)

--- module/base/base.go
+++ module/base/base.go
@@ -2,6 +2,7 @@ package base
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
 	"encoding/hex"
 	"strings"
 )
@@ -11,6 +12,13 @@ func EncodeMD5(str string) string {
 	m := md5.New()
 	m.Write([]byte(str))
 	return hex.EncodeToString(m.Sum(nil))
+}
+
+// EncodeSha1 string to sha1 hex value.
+func EncodeSha1(str string) string {
+	h := sha1.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
 }
 
 // HashEmail hashes email address to MD5 string.
