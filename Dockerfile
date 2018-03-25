bdd3f44363435b818c3fa1b40852497c4aee28f1
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-03-25 20:31:20 +0800

    feat: support healthy check for docker container.

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,12 @@ FROM plugins/base:multiarch
 
 LABEL maintainer="Bo-Yi Wu <appleboy.tw@gmail.com>"
 
-EXPOSE 8080 8081 80 443
+EXPOSE 8080 8081
 
 ADD bin/ggz /
+
+HEALTHCHECK --start-period=2s --interval=10s --timeout=5s \
+  CMD ["/ggz", "ping"]
 
 ENTRYPOINT ["/ggz"]
 CMD ["server"]
---
+++ cmd/ping.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"gopkg.in/urfave/cli.v2"
+)
+
+// Ping provides the sub-command to check server live.
+func Ping() *cli.Command {
+	return &cli.Command{
+		Name:  "ping",
+		Usage: "server healthy check",
+		Action: func(c *cli.Context) error {
+			resp, err := http.Get("http://localhost:8080/healthz")
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode != 200 {
+				return fmt.Errorf("server returned non-200 status code")
+			}
+			return nil
+		},
+	}
+}
