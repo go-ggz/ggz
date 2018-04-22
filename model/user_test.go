f690657ac9f121289d380b370c3333a93915c4d0
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-04-22 20:35:15 +0800

    test: Add GetUserByEmail testing

--- model/user_test.go
+++ model/user_test.go
@@ -24,3 +24,28 @@ func TestIsUserExist(t *testing.T) {
 	assert.NoError(t, err)
 	assert.False(t, exists)
 }
+
+func TestGetUserByEmail(t *testing.T) {
+	assert.NoError(t, PrepareTestDatabase())
+
+	t.Run("missing email", func(t *testing.T) {
+		user, err := GetUserByEmail("")
+		assert.Error(t, err)
+		assert.Nil(t, user)
+		assert.True(t, IsErrUserNotExist(err))
+	})
+
+	t.Run("test exist email", func(t *testing.T) {
+		user, err := GetUserByEmail("test@gmail.com")
+		assert.NoError(t, err)
+		assert.NotNil(t, user)
+		assert.Equal(t, int64(1), user.ID)
+	})
+
+	t.Run("email not found", func(t *testing.T) {
+		user, err := GetUserByEmail("test123456@gmail.com")
+		assert.Error(t, err)
+		assert.Nil(t, user)
+		assert.True(t, IsErrUserNotExist(err))
+	})
+}
