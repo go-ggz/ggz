deb5232b2b31931987fac940d319f82da145efb4
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-04-28 13:43:47 +0800

    test: update query url

--- schema/url_test.go
+++ schema/url_test.go
@@ -14,49 +14,49 @@ func TestQueryShortenURL(t *testing.T) {
 	assert.NoError(t, model.PrepareTestDatabase())
 	user, _ := model.GetUserByID(1)
 	ctx := newContextWithUser(context.TODO(), user)
-	test := T{
-		Query: `
-		{
-      QueryShortenURL(slug: "abcdef") {
-        url
-      }
-    }
+	t.Run("shorten url exist", func(t *testing.T) {
+		test := T{
+			Query: `
+{
+  QueryShortenURL(slug: "abcdef") {
+    url
+  }
+}
 	  `,
-		Schema: Schema,
-		Expected: &graphql.Result{
-			Data: map[string]interface{}{
-				"QueryShortenURL": map[string]interface{}{
-					"url": "http://example.com",
+			Schema: Schema,
+			Expected: &graphql.Result{
+				Data: map[string]interface{}{
+					"QueryShortenURL": map[string]interface{}{
+						"url": "http://example.com",
+					},
 				},
 			},
-		},
-	}
-	params := graphql.Params{
-		Schema:        test.Schema,
-		RequestString: test.Query,
-		Context:       ctx,
-	}
-	testGraphql(test, params, t)
-}
+		}
+		params := graphql.Params{
+			Schema:        test.Schema,
+			RequestString: test.Query,
+			Context:       ctx,
+		}
+		testGraphql(test, params, t)
+	})
 
-func TestQueryShortenURLNotFound(t *testing.T) {
-	assert.NoError(t, model.PrepareTestDatabase())
-	user, _ := model.GetUserByID(1)
-	ctx := newContextWithUser(context.TODO(), user)
-	test := T{
-		Query: `
+	t.Run("shorten url not exist", func(t *testing.T) {
+		test := T{
+			Query: `
 {
   QueryShortenURL(slug: "1234567890") {
     url
   }
 }
 `,
-		Schema: Schema,
-	}
-	params := graphql.Params{
-		Schema:        test.Schema,
-		RequestString: test.Query,
-		Context:       ctx,
-	}
-	testGraphqlErr(test, params, t)
+			Schema: Schema,
+		}
+		params := graphql.Params{
+			Schema:        test.Schema,
+			RequestString: test.Query,
+			Context:       ctx,
+		}
+		testGraphqlErr(test, params, t)
+	})
+
 }
