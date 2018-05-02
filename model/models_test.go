df6d53f283fcdfa8b7bea06f9c8ff0febd2767e5
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-05-02 13:39:50 +0800

    refactor: update pg testing

--- model/models_test.go
+++ model/models_test.go
@@ -8,44 +8,55 @@ import (
 
 func TestParsePostgreSQLHostPort(t *testing.T) {
 	tests := []struct {
+		Name     string
 		HostPort string
 		Host     string
 		Port     string
 	}{
 		{
+			Name:     "ip and port",
 			HostPort: "127.0.0.1:1234",
 			Host:     "127.0.0.1",
 			Port:     "1234",
 		},
 		{
+			Name:     "ip",
 			HostPort: "127.0.0.1",
 			Host:     "127.0.0.1",
 			Port:     "5432",
 		},
 		{
+			Name:     "ipv6 and port",
 			HostPort: "[::1]:1234",
 			Host:     "[::1]",
 			Port:     "1234",
 		},
 		{
+			Name:     "ipv6",
 			HostPort: "[::1]",
 			Host:     "[::1]",
 			Port:     "5432",
 		},
 		{
+			Name:     "socket and port",
 			HostPort: "/tmp/pg.sock:1234",
 			Host:     "/tmp/pg.sock",
 			Port:     "1234",
 		},
 		{
+			Name:     "socket",
 			HostPort: "/tmp/pg.sock",
 			Host:     "/tmp/pg.sock",
 			Port:     "5432",
 		},
 	}
-	for _, test := range tests {
-		host, port := parsePostgreSQLHostPort(test.HostPort)
-		assert.Equal(t, test.Host, host)
-		assert.Equal(t, test.Port, port)
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+			host, port := parsePostgreSQLHostPort(tt.HostPort)
+			assert.Equal(t, tt.Host, host)
+			assert.Equal(t, tt.Port, port)
+		})
 	}
 }
