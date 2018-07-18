dabc762755868aa471e81a5d7a15b900f07d0f9b
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-07-18 11:49:46 +0800

    refactor: code

--- schema/schema.go
+++ schema/schema.go
@@ -4,26 +4,29 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
-var rootQuery = graphql.NewObject(graphql.ObjectConfig{
-	Name:        "RootQuery",
-	Description: "Root Query",
-	Fields: graphql.Fields{
-		"QueryURLMetadata":   &queryURLMeta,
-		"QueryShortenURL":    &queryShortenURL,
-		"QueryAllShortenURL": &queryAllShortenURL,
-	},
-})
+var rootQuery = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name:        "RootQuery",
+		Description: "Root Query",
+		Fields: graphql.Fields{
+			"QueryURLMetadata":   &queryURLMeta,
+			"QueryShortenURL":    &queryShortenURL,
+			"QueryAllShortenURL": &queryAllShortenURL,
+		},
+	})
 
-var rootMutation = graphql.NewObject(graphql.ObjectConfig{
-	Name:        "RootMutation",
-	Description: "Root Mutation",
-	Fields: graphql.Fields{
-		"CreateShortenURL": &createShortenURL,
-	},
-})
+var rootMutation = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name:        "RootMutation",
+		Description: "Root Mutation",
+		Fields: graphql.Fields{
+			"CreateShortenURL": &createShortenURL,
+		},
+	})
 
 // Schema is the GraphQL schema served by the server.
-var Schema, _ = graphql.NewSchema(graphql.SchemaConfig{
-	Query:    rootQuery,
-	Mutation: rootMutation,
-})
+var Schema, _ = graphql.NewSchema(
+	graphql.SchemaConfig{
+		Query:    rootQuery,
+		Mutation: rootMutation,
+	})
