44bfb24708e0fc93e1c7bb9237da07ce85cf501f
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-09-02 18:59:29 +0800

    fix: check jwt type.

--- helper/jwt.go
+++ helper/jwt.go
@@ -11,10 +11,18 @@ import (
 
 // GetUserDataFromToken from jwt parse token
 func GetUserDataFromToken(ctx context.Context) jwt.MapClaims {
+	if _, ok := ctx.Value("user").(*jwt.Token); !ok {
+		return jwt.MapClaims{}
+	}
+
 	return ctx.Value("user").(*jwt.Token).Claims.(jwt.MapClaims)
 }
 
 // GetUserDataFromModel from user model
 func GetUserDataFromModel(ctx context.Context) *model.User {
+	if _, ok := ctx.Value(config.ContextKeyUser).(*model.User); !ok {
+		return nil
+	}
+
 	return ctx.Value(config.ContextKeyUser).(*model.User)
 }
