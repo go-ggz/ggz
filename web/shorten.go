d6d823efd8f81d5b283b5c5277e72532cb39e1df
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-09-14 15:41:20 +0800

    fix: use regexp.MustCompile

--- web/shorten.go
+++ web/shorten.go
@@ -16,6 +16,8 @@ import (
 	"github.com/skip2/go-qrcode"
 )
 
+var shortenPattern = regexp.MustCompile(`^[a-zA-Z0-9]+$`)
+
 // ShortenedIndex index page.
 func ShortenedIndex(c *gin.Context) {
 	c.Redirect(http.StatusMovedPermanently, config.Server.Host)
@@ -72,21 +74,9 @@ func CreateShortenURL(c *gin.Context) {
 
 // FetchShortenedURL show URL content
 func FetchShortenedURL(c *gin.Context) {
-	r, err := regexp.Compile(`^[a-zA-Z0-9]+$`)
-	if err != nil {
-		c.JSON(
-			http.StatusBadRequest,
-			gin.H{
-				"code":  http.StatusBadRequest,
-				"error": "regexp not correct",
-			},
-		)
-		return
-	}
-
 	slug := c.Param("slug")
 
-	if !r.MatchString(slug) {
+	if !shortenPattern.MatchString(slug) {
 		errorJSON(c, http.StatusBadRequest, errSlugNotMatch)
 		return
 	}
