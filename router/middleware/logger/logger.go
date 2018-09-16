dec961ff9f8c930f5043a07c38096f0bece91be5
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-09-17 00:13:59 +0800

    refactor(logger): using zero log library

--- router/middleware/logger/logger.go
+++ router/middleware/logger/logger.go
@@ -4,7 +4,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
+	"github.com/rs/zerolog/log"
 )
 
 // SetLogger initializes the logging middleware.
@@ -12,26 +12,53 @@ func SetLogger() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
 		path := c.Request.URL.Path
+		raw := c.Request.URL.RawQuery
+		if raw != "" {
+			path = path + "?" + raw
+		}
 
 		c.Next()
 
 		end := time.Now()
 		latency := end.Sub(start)
 
-		entry := logrus.WithFields(logrus.Fields{
-			"status":     c.Writer.Status(),
-			"method":     c.Request.Method,
-			"path":       path,
-			"ip":         c.ClientIP(),
-			"latency":    latency,
-			"user-agent": c.Request.UserAgent(),
-			"time":       end.Format(time.RFC3339),
-		})
+		msg := "Request"
+		if c.Errors.String() != "" {
+			msg = c.Errors.String()
+		}
 
-		if len(c.Errors) > 0 {
-			entry.Error(c.Errors.String())
-		} else {
-			entry.Info("requested")
+		switch {
+		case c.Writer.Status() >= 400 && c.Writer.Status() < 500:
+			{
+				log.Warn().
+					Int("status", c.Writer.Status()).
+					Str("method", c.Request.Method).
+					Str("path", path).
+					Str("ip", c.ClientIP()).
+					Dur("latency", latency).
+					Str("user-agent", c.Request.UserAgent()).
+					Msg(msg)
+			}
+		case c.Writer.Status() >= 500:
+			{
+				log.Error().
+					Int("status", c.Writer.Status()).
+					Str("method", c.Request.Method).
+					Str("path", path).
+					Str("ip", c.ClientIP()).
+					Dur("latency", latency).
+					Str("user-agent", c.Request.UserAgent()).
+					Msg(msg)
+			}
+		default:
+			log.Info().
+				Int("status", c.Writer.Status()).
+				Str("method", c.Request.Method).
+				Str("path", path).
+				Str("ip", c.ClientIP()).
+				Dur("latency", latency).
+				Str("user-agent", c.Request.UserAgent()).
+				Msg(msg)
 		}
 	}
 }
