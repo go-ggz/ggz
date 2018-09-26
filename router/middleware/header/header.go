0a2601686fb68bbafc4c5a1bc4041b6991cde87a
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-09-26 21:24:29 +0800

    // Using 204 is better than 200 when the request status is OPTIONS
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- router/middleware/header/header.go
+++ router/middleware/header/header.go
@@ -1,6 +1,8 @@
 package header
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,7 +18,7 @@ func Options(c *gin.Context) {
 		c.Header("Access-Control-Allow-Headers", "authorization, origin, content-type, accept")
 		c.Header("Allow", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
 		c.Header("Content-Type", "application/json")
-		c.AbortWithStatus(200)
+		c.AbortWithStatus(http.StatusNoContent)
 	}
 }
 
