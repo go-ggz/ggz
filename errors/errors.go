bb0d13f85b01918b526781a7a904e645ac856ba3
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-11-13 11:35:12 +0800

    feat(errors): support custom graphql errors.

---
+++ errors/errors.go
@@ -0,0 +1,143 @@
+package errors
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Error applicational
+type Error struct {
+	Type    Type
+	Message string
+	cause   error
+}
+
+// Error message
+func (e *Error) Error() string {
+	return e.Message
+}
+
+// Cause of the original error
+func (e *Error) Cause() string {
+	if e.cause != nil {
+		return e.cause.Error()
+	}
+
+	return ""
+}
+
+// Extensions for graphQL extension
+func (e *Error) Extensions() map[string]interface{} {
+	return map[string]interface{}{
+		"code": e.Type.Code(),
+		"type": e.Type,
+	}
+}
+
+// Type defines the type of an error
+type Type string
+
+const (
+	// Internal error
+	Internal Type = "internal"
+	// NotFound error means that a specific item does not exist
+	NotFound Type = "not_found"
+	// BadRequest error
+	BadRequest Type = "bad_request"
+	// Validation error
+	Validation Type = "validation"
+	// AlreadyExists error
+	AlreadyExists Type = "already_exists"
+	// Unauthorized error
+	Unauthorized Type = "unauthorized"
+)
+
+func (t Type) String() string {
+	switch t {
+	case Internal:
+		return "Internal Error"
+	case NotFound:
+		return "Item not found"
+	case BadRequest:
+		return "BadRequest error"
+	case Validation:
+		return "Validation error"
+	case AlreadyExists:
+		return "Item already exists"
+	case Unauthorized:
+		return "Unauthorized error"
+	}
+
+	return "Unknown error"
+}
+
+// Code http error code
+func (t Type) Code() int {
+	switch t {
+	case Internal:
+		return http.StatusInternalServerError
+	case NotFound:
+		return http.StatusNotFound
+	case BadRequest:
+		return http.StatusBadRequest
+	case Validation:
+		return http.StatusForbidden
+	case AlreadyExists:
+		return http.StatusBadRequest
+	case Unauthorized:
+		return http.StatusUnauthorized
+	}
+
+	return http.StatusInternalServerError
+}
+
+// New creates a new error
+func New(t Type, msg string, err error) error {
+	return &Error{
+		Type:    t,
+		Message: msg,
+		cause:   err,
+	}
+}
+
+// EValidation creates an error of type Validationn
+func EValidation(msg string, err error, arg ...interface{}) error {
+	return New(Validation, fmt.Sprintf(msg, arg...), err)
+}
+
+// ENotExists creates an error of type NotExist
+func ENotExists(msg string, err error, arg ...interface{}) error {
+	return New(NotFound, fmt.Sprintf(msg, arg...), err)
+}
+
+// EBadRequest creates an error of type BadRequest
+func EBadRequest(msg string, err error, arg ...interface{}) error {
+	return New(BadRequest, fmt.Sprintf(msg, arg...), err)
+}
+
+// EAlreadyExists creates an error of type EAlreadyExistsl
+func EAlreadyExists(msg string, err error, arg ...interface{}) error {
+	return New(AlreadyExists, fmt.Sprintf(msg, arg...), err)
+}
+
+// EInternal creates an error of type Internal
+func EInternal(msg string, err error, arg ...interface{}) error {
+	return New(Internal, fmt.Sprintf(msg, arg...), err)
+}
+
+// ENotFound creates an error of type NotFound
+func ENotFound(msg string, err error, arg ...interface{}) error {
+	return New(NotFound, fmt.Sprintf(msg, arg...), err)
+}
+
+// EUnauthorized creates an error of type Unauthorized
+func EUnauthorized(msg string, err error, arg ...interface{}) error {
+	return New(Unauthorized, fmt.Sprintf(msg, arg...), err)
+}
+
+// Is method checks if an error is of a specific type
+func Is(t Type, err error) bool {
+	e, ok := err.(*Error)
+
+	return ok && e.Type == t
+}
---
+++ errors/errors_test.go
@@ -0,0 +1,120 @@
+package errors
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIs(t *testing.T) {
+	type args struct {
+		t   Type
+		err error
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Internal check",
+			args: args{Internal, EInternal("Internal error", nil)},
+			want: true,
+		},
+		{
+			name: "NotFound check",
+			args: args{NotFound, ENotFound("NotFound error", nil)},
+			want: true,
+		},
+		{
+			name: "BadRequest check",
+			args: args{BadRequest, EBadRequest("BadRequest error", nil)},
+			want: true,
+		},
+		{
+			name: "Validation check",
+			args: args{Validation, EValidation("Validation error", nil)},
+			want: true,
+		},
+		{
+			name: "EAlreadyExists check",
+			args: args{AlreadyExists, EAlreadyExists("EAlreadyExists error", nil)},
+			want: true,
+		},
+		{
+			name: "EUnauthorized check",
+			args: args{Unauthorized, EUnauthorized("EUnauthorized error", nil)},
+			want: true,
+		},
+		{
+			name: "new error check",
+			args: args{Internal, New(Internal, "error", nil)},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Is(tt.args.t, tt.args.err); got != tt.want {
+				t.Errorf("Is() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestErrorType(t *testing.T) {
+	t.Run("EValidation", func(t *testing.T) {
+		err := EValidation("not validation", errors.New("test"))
+		assert.Equal(t, "not validation", err.Error())
+		assert.Equal(t, "test", err.(*Error).Cause())
+		assert.Equal(t, http.StatusForbidden, err.(*Error).Type.Code())
+		assert.Equal(t, "Validation error", err.(*Error).Type.String())
+	})
+
+	t.Run("EBadRequest", func(t *testing.T) {
+		err := EBadRequest("not EBadRequest", nil)
+		assert.Equal(t, "not EBadRequest", err.Error())
+		assert.Equal(t, "", err.(*Error).Cause())
+		assert.Equal(t, http.StatusBadRequest, err.(*Error).Type.Code())
+		assert.Equal(t, "BadRequest error", err.(*Error).Type.String())
+	})
+
+	t.Run("EAlreadyExists", func(t *testing.T) {
+		err := EAlreadyExists("not EAlreadyExists", errors.New("test"))
+		assert.Equal(t, "not EAlreadyExists", err.Error())
+		assert.Equal(t, "test", err.(*Error).Cause())
+		assert.Equal(t, http.StatusBadRequest, err.(*Error).Type.Code())
+		assert.Equal(t, "Item already exists", err.(*Error).Type.String())
+	})
+
+	t.Run("EInternal", func(t *testing.T) {
+		err := EInternal("not EInternal", errors.New("test"))
+		assert.Equal(t, "not EInternal", err.Error())
+		assert.Equal(t, "test", err.(*Error).Cause())
+		assert.Equal(t, http.StatusInternalServerError, err.(*Error).Type.Code())
+		assert.Equal(t, "Internal Error", err.(*Error).Type.String())
+	})
+
+	t.Run("ENotFound", func(t *testing.T) {
+		err := ENotFound("not validation", errors.New("test"))
+		assert.Equal(t, "not validation", err.Error())
+		assert.Equal(t, "test", err.(*Error).Cause())
+		assert.Equal(t, http.StatusNotFound, err.(*Error).Type.Code())
+		assert.Equal(t, "Item not found", err.(*Error).Type.String())
+	})
+
+	t.Run("EUnauthorized", func(t *testing.T) {
+		err := EUnauthorized("not validation", errors.New("test"))
+		assert.Equal(t, "not validation", err.Error())
+		assert.Equal(t, "test", err.(*Error).Cause())
+		assert.Equal(t, http.StatusUnauthorized, err.(*Error).Type.Code())
+		assert.Equal(t, "Unauthorized error", err.(*Error).Type.String())
+	})
+
+	t.Run("Unknown Error", func(t *testing.T) {
+		unknown := Type("Unknown")
+		assert.Equal(t, http.StatusInternalServerError, unknown.Code())
+		assert.Equal(t, "Unknown error", unknown.String())
+	})
+}
