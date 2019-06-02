b0cfbe9154f5c0c7d7514ad54d96875c824c7a25
Bo-Yi Wu <appleboy.tw@gmail.com>
2019-06-03 00:09:09 +0800

    refactor: update ping command

--- cmd/ggz-redirect/ping.go
+++ cmd/ggz-redirect/ping.go
@@ -4,24 +4,50 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-ggz/ggz/config"
+
+	"github.com/rs/zerolog/log"
 	"gopkg.in/urfave/cli.v2"
 )
+
+func healthAction() cli.ActionFunc {
+	return func(c *cli.Context) error {
+		resp, err := http.Get("http://localhost" + config.Server.Addr + "/healthz")
+		if err != nil {
+			log.Error().
+				Err(err).
+				Msg("failed to request health check")
+			return err
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			log.Error().
+				Int("code", resp.StatusCode).
+				Msg("health seems to be in bad state")
+			return fmt.Errorf("server returned non-200 status code")
+		}
+		return nil
+	}
+}
+
+func healthFlags() []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:        "addr",
+			Value:       defaultHostAddr,
+			Usage:       "Address to bind the server",
+			EnvVars:     []string{"GGZ_SERVER_ADDR"},
+			Destination: &config.Server.Addr,
+		},
+	}
+}
 
 // Ping provides the sub-command to check server live.
 func Ping() *cli.Command {
 	return &cli.Command{
-		Name:  "ping",
-		Usage: "server healthy check",
-		Action: func(c *cli.Context) error {
-			resp, err := http.Get("http://localhost" + defaultHostAddr + "/healthz")
-			if err != nil {
-				return err
-			}
-			defer resp.Body.Close()
-			if resp.StatusCode != http.StatusOK {
-				return fmt.Errorf("server returned non-200 status code")
-			}
-			return nil
-		},
+		Name:   "ping",
+		Usage:  "perform health checks",
+		Flags:  healthFlags(),
+		Action: healthAction(),
 	}
 }
