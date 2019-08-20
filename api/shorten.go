e2c149ee8808ffbe88e50fae0a598e491da069d7
Bo-Yi Wu <appleboy.tw@gmail.com>
2019-08-20 23:38:09 +0800

    fix: fmt

--- api/shorten.go
+++ api/shorten.go
@@ -8,9 +8,9 @@ import (
 
 	"github.com/go-ggz/ggz/pkg/config"
 	"github.com/go-ggz/ggz/pkg/helper"
-	"github.com/go-ggz/ggz/pkg/router"
 	"github.com/go-ggz/ggz/pkg/model"
 	"github.com/go-ggz/ggz/pkg/module/storage"
+	"github.com/go-ggz/ggz/pkg/router"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
